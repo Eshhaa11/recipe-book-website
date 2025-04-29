58751e7a9d5a805e8acd339a8ab3a239bd1bf03f
Esha Patel <Patellesha11@gmail.com>
2025-04-29 21:35:50 +0300

    added recipes

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -2,3 +2,8 @@ import { useState } from "react";
 import "./Recipe.css";
 
 const recipes = [
+    {
+        title: "Spaghetti Carbonara",
+        ingredients: ["Spaghetti", "Eggs", "Parmesan", "Pancetta", "Black pepper"],
+        steps: ["Boil pasta", "Fry pancetta", "Mix eggs and cheese", "Combine all"],
+      },
